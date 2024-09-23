f1e4d48c9f31204f62f899d137e8de08ad6667c1
Stig B. Dørmænen <stigbd@gmail.com>
2024-09-23 15:37:40 +0200

    Implement consumer in java
    Signed-off-by: Stig B. Dørmænen <stigbd@gmail.com>

---
+++ hello-kafka-java/src/test/java/com/example/hello_kafka_java/HelloKafkaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_kafka_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloKafkaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
